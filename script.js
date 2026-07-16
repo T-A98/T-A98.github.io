1afe926c21a38f958139af9111ae32355e762950
Fliegemann <t.allgeier98@gmail.com>
2026-07-16 21:13:03 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const endDate = new Date("nov 25, 2026 00:00:00").getTime();
+
+const interval = setInterval(() => {
+  const currentDate = new Date(),
+    timeLeft = endDate - currentDate.getTime(), // in ms
+    daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate(); 
+
+  if (timeLeft <= 0) {
+    clearInterval(x);
+    return;
+  }
+
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000),
+    minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)),
+    hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    days = daysInMonth - currentDate.getDate(),
+    months = Math.floor(timeLeft / (1000 * 60 * 60 * 24 * daysInMonth));
+
+  // Get elements by class name
+  let countdownElements = document.getElementsByClassName("countdown-element");
+
+  // Loop through the elements and update their content
+  for (const countdownElement of countdownElements) {
+    let className = countdownElement.classList[1]; // Get the second class name
+    switch (className) {
+      case "months":
+        countdownElement.innerHTML = months
+        break
+      case "days":
+        countdownElement.innerHTML = days;
+        break;
+      case "hours":
+        countdownElement.innerHTML = hours;
+        break;
+      case "minutes":
+        countdownElement.innerHTML = minutes;
+        break;
+      case "seconds":
+        countdownElement.innerHTML = seconds;
+        break;
+      default:
+        break;
+    }
+  }
+}, 1000);
